fd58015d0599aa0170c611cab4a2b184c57d8e3d
Johnny Huang <johnny914425@gmail.com>
2016-12-06 23:57:56 +0800

    可以一對一聊天

--- libs/socketIO.js
+++ libs/socketIO.js
@@ -1,13 +1,30 @@
 "use strict";
 var debug = require("debug")("dev");
 var self = module.exports;
+var userStatus = {};
 
 self.IO = function ( io) {
   io.on( 'connection', function (socket) {
-    debug( 'user: ' + socket.id + ' in');
+    socket.on( 'regist', function (data){
+      if ( data && data.secret && !userStatus[data.secret]){
+        userStatus[data.secret] = [];
+        userStatus[data.secret].push( socket.id);
+      } else if ( data && data.secret ) {
+        userStatus[data.secret].push( socket.id);
+      }
+      debug( userStatus);
+    });
     // 在Cache加上，每個User的socket
-    socket.on( 'msg', function (data) {
-      console.log(data);
+    socket.on( 'msgIn', function (data) {
+      debug(data);
+      if ( data && data.target && data.msg){
+        var target = userStatus[data.target];
+        // socket.emit( "msgOut", data);
+        for (var i = 0, imax = target.length; i < imax; i+=1){
+          debug( target[i], userStatus, data.msg);
+          io.to([target[i]]).emit( "msgOut", { from: data.from, msg: data.msg});
+        }
+      }
     });
     socket.on( 'disconnect', function (){
       debug( 'user: ' + socket.id + ' leave');
--- routes/web.js
+++ routes/web.js
@@ -3,17 +3,17 @@ var debug = require("debug")("dev");
 var express = require('express');
 var router = express.Router();
 var auth = require( __dirname + '/../Libs/auth');
-router.get( '/login', function ( req, res, next){
+router.get( '/login', function ( req, res){
   res.render( 'login');
 });
 
-router.post( '/login', function ( req, res, next){
+router.post( '/login', function ( req, res){
   var data = req.body;
   auth.login( data.username, data.password, function ( doc) {
-    debug(doc);
+    // debug(doc);
     if ( doc){
       req.session.user = {
-        _id: doc._id,
+        _id: doc._id.toString(),
         email: doc.email,
         name: doc.name
       };
@@ -21,11 +21,11 @@ router.post( '/login', function ( req, res, next){
     return res.send({ code: 200, data: doc});
   });
 });
-router.get( '/home', lr, function(req, res, next) {
-  res.render( 'chat');
-  
-  debug(req.session.user)
+router.get( '/home', lr, function(req, res) {
+  res.render( 'chat', { user: req.session.user});
+  debug(req.session.user);
 });
+
 module.exports = router;
 
 function lr(req, res, next){
