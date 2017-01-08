09859e48c9b7b557463c6c858109354f3deabb6d
Johnny Huang <johnny914425@gmail.com>
2017-01-08 15:35:52 +0800

    chat escape html

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,5 @@
 /*eslint-env browser*/
-/* globals $SCROLL, SOCKET, USER*/
+/* globals $SCROLL, SOCKET, USER, ENV*/
 $(function (){
   // $SCROLL.scrollTop( $SCROLL[0].scrollHeight);
   SOCKET.on("connect", function (){
@@ -7,11 +7,15 @@ $(function (){
       SOCKET.emit( 'regist', { secret: USER});
     }
   });
+
   SOCKET.on('msgOut', function (data) {
     receive(data);
   });
+
   SOCKET.on('disconnect', function (){
-    document.location.href="/login";
+    if ( !USER && ENV === "production") {
+      document.location.href="/login";
+    }
   });
 
   $(".msgEnter input").keypress( function (e){
@@ -22,22 +26,27 @@ $(function (){
       $this.val("");
     }
   });
+
   $(".friends").on( "click", function(){
     var $this = $(this);
     $(".msgEnter input").data("target", $this.data("target")).attr("placeholder", "Write a message...").attr("disabled",false);
-    console.log("switch target to " + $this.data("target"));
+    $(".block-content .content").css("display", "none");
+    $("#"+$this.data("target")).css("display", "block");
   });
 });
 
 function sendMsg( SOCKET, msg, target) {
-  var from = "<li class=\"row from\"><span class=\"bubble\">"+ msg +"</span></li>";
-  $(".content ul").append(from);
-  $SCROLL.scrollTop( $SCROLL[0].scrollHeight);
+  var from = "<li class=\"row from\"><span class=\"bubble\"></span></li>";
+  var $target = $("#"+ target);
+  $target.find("ul").append(from).find(".from .bubble:last").text(msg);
+  $target.scrollTop( $target[0].scrollHeight);
   SOCKET.emit('msgIn', { target: target, from: USER, msg: msg});
 }
+
 function receive(data){
-  var to = "<li class=\"row to\"><span class=\"bubble\">"+ data.msg +"</span></li>";
-  $(".content ul").append(to);
-  $SCROLL.scrollTop( $SCROLL[0].scrollHeight);
+  var to = "<li class=\"row to\"><span class=\"bubble\"></span></li>";
+  var $from = $("#"+ data.from);
+  $from.find("ul").append(to).find(".to .bubble:last").text(data.msg);
+  $from.scrollTop( $from[0].scrollHeight);
 }
 
