bb6e0e6bcc6dd5b3546d52cd235af31dba79d2c2
Johnny Huang <johnny914425@gmail.com>
2017-01-09 23:47:28 +0800

    favicon-serve enable

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ global.db;
 var debug = require('debug')('dev:app.js');
 var express = require('express');
 var path = require('path');
-// var favicon = require('serve-favicon');
+var favicon = require('serve-favicon');
 // var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -24,7 +24,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join( __dirname, 'public/images', 'favicon.ico')));
 // app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
